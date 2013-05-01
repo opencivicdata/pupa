61aeab2e148ecb016213ce9a252ef45fcec9381b
James Turk <james.p.turk@gmail.com>
2013-05-01 11:50:33 -0400

    basic bill import

---
+++ pupa/importers/bills.py
@@ -0,0 +1,37 @@
+from pupa.core import db
+from pupa.utils import fix_bill_id
+from .base import BaseImporter
+
+
+class BillImporter(BaseImporter):
+    _type = 'bill'
+
+    def get_db_spec(self, bill):
+        spec = {'jurisdiction_id': bill['jurisdiction_id'],
+                'session': bill['session'],
+                'bill_id': bill['bill_id'],
+               }
+        if 'chamber' in bill:
+            spec['chamber'] = bill['chamber']
+
+    def prepare_object_from_json(self, obj):
+        obj['bill_id'] = fix_bill_id(obj['bill_id'])
+        if 'alternate_bill_ids' in obj:
+            obj['alternate_bill_ids'] = [fix_bill_id(bid) for bid in
+                                         obj['alternate_bill_ids']]
+
+        # XXX: subject categorizer
+        # XXX: action categorizer
+
+        for rel in obj['related_bills']:
+            rel['bill_id'] = fix_bill_id(rel['bill_id'])
+            spec = rel.copy()
+            spec['jurisdiction_id'] = obj['jurisdiction_id']
+            rel_obj = db.bills.find_one(spec)
+            if rel_obj:
+                rel['internal_id'] = rel_obj['_id']
+            else:
+                self.logger.warning('Unknown companion: {chamber} {session} '
+                                    '{bill_id}'.format(**rel))
+
+        return obj
--- pupa/utils/__init__.py
+++ pupa/utils/__init__.py
@@ -1,3 +1,4 @@
+import re
 import os
 import time
 import json
@@ -9,6 +10,19 @@ from bson import ObjectId
 def makedirs(dname):
     if not os.path.isdir(dname):
         os.makedirs(dname)
+
+
+# fixing bill ids
+_bill_id_re = re.compile(r'([A-Z]*)\s*0*([-\d]+)')
+_mi_bill_id_re = re.compile(r'(SJR|HJR)\s*([A-Z]+)')
+
+
+def fix_bill_id(bill_id):
+    bill_id = bill_id.replace('.', '')
+    # special case for MI Joint Resolutions
+    if _mi_bill_id_re.match(bill_id):
+        return _mi_bill_id_re.sub(r'\1 \2', bill_id, 1).strip()
+    return _bill_id_re.sub(r'\1 \2', bill_id, 1).strip()
 
 
 class DatetimeValidator(SchemaValidator):
