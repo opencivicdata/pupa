74b377e3668127bc6b94007958bc51af4a5bee51
Paul Tagliamonte <tag@pault.ag>
2013-05-31 17:02:52 -0400

    add db spec

--- pupa/importers/votes.py
+++ pupa/importers/votes.py
@@ -4,5 +4,13 @@ from .base import BaseImporter
 class VoteImporter(BaseImporter):
     _type = 'vote'
 
+    def get_db_spec(self, event):
+        spec = {
+            "motion": event['motion'],
+            "chamber": event['chamber'],
+            "date": event['date'],
+        }
+        return spec
+
     def prepare_object_from_json(self, obj):
         return obj
