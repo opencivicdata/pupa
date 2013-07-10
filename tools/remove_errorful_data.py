7516c5a19d7455fb2da086ae7057a92ef00a7020
Paul Tagliamonte <tag@pault.ag>
2013-07-10 15:38:21 -0400

    add sponsor purger.

--- tools/remove_errorful_data.py
+++ tools/remove_errorful_data.py
@@ -6,8 +6,8 @@ from pupa.core import db
 
 
 fatal_errors = [
-    "event-has-invalid-jurisdiction-id",
-    "vote-has-invalid-jurisdiction-id",
+    "event-has-unlinked-jurisdiction-id",
+    "vote-has-unlinked-jurisdiction-id",
 ]
 
 
@@ -16,7 +16,21 @@ with open(*sys.argv[1:], mode='r') as fd:
 
 
 for collection, entries in data.items():
+    cdb = getattr(db, collection)
     for datum in entries:
         if datum['tagname'] in fatal_errors:
-            getattr(db, collection).remove(datum['id'], safe=True)
+            cdb.remove(datum['id'], safe=True)
             print datum['id'], "removed"
+
+        if datum['tagname'] == 'bad-sponsor-id':
+            one = cdb.find_one({"_id": datum['id']})
+            if one is None:
+                continue
+
+            bsid = datum['data']['sponsor-id']
+
+            for sponsor in one['sponsors']:
+                if sponsor['id'] == bsid:
+                    sponsor['id'] = None
+                    print one['_id'], "removed bad sponsor"
+            cdb.save(one)
