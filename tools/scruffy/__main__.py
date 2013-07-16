82e2bb096eb96ffa59f506d20ac48e4d1142acd5
Paul Tagliamonte <tag@pault.ag>
2013-07-16 15:26:45 -0400

    add argparse

--- tools/scruffy/__main__.py
+++ tools/scruffy/__main__.py
@@ -1,12 +1,26 @@
-from . import db
 from .checkers import load_checkers
 from pupa.utils import JSONEncoderPlus
+from pymongo import Connection
 
 from collections import defaultdict
+import argparse
 import json
 
 
 report = defaultdict(list)
+
+parser = argparse.ArgumentParser(description='Check an OCD Database.')
+parser.add_argument('--server', type=str, help='OCD Mongo Server',
+                    default="localhost")
+parser.add_argument('--database', type=str, help='OCD Mongo Database',
+                    default="ocd")
+parser.add_argument('--port', type=int, help='OCD Mongo Server Port',
+                    default=27017)
+
+args = parser.parse_args()
+
+connection = Connection(args.server, args.port)
+db = getattr(connection, args.database)
 
 
 for checker in load_checkers():
