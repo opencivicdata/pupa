b1f76e1bb303f05f54a8cbbcd8b9d17ab067f7c2
Paul Tagliamonte <tag@pault.ag>
2013-07-24 14:44:53 -0400

    Minor cleanup; initial org matcher.

--- pupa/utils/merge.py
+++ pupa/utils/merge.py
@@ -2,8 +2,6 @@ from pupa.core import db
 
 
 def match_membership(membership, other_entity):
-    unmatched = membership.pop("unmatched_legislator")
-
     pid = membership.get('person_id')
     oid = membership.get('organization_id')
 
@@ -14,6 +12,7 @@ def match_membership(membership, other_entity):
         raise ValueError("Attempting to match a hopeless membership "
                          "(no org id or person id)")
 
+    unmatched = membership.pop("unmatched_legislator")
 
     def match_person(membership, person):
         if person['_type'] != 'person':
@@ -35,15 +34,12 @@ def match_membership(membership, other_entity):
         if org['_type'] != 'organization':
             raise ValueError("Matching an organization, but not given an "
                              "organization.")
+        membership['organization_id'] = org['_id']
+
+        db.organizations.save(org)
+        db.memberships.save(membership)
+        return (membership, person)
 
     if pid is None:
         return match_person(membership, other_entity)
     return match_org(membership, other_entity)
-
-
-p = db.people.find_one({"_id": "ocd-person/b195d812-efe5-11e2-8334-f0def1bd7298"})
-m = db.memberships.find_one({"_id": "ocd-membership/afec32ae-efe5-11e2-9936-f0def1bd7298"})
-
-
-#print p, m
-print match_membership(m, p)
