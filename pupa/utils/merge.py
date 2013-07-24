13663df9699abd8f3c98128afce878270ec682c5
Paul Tagliamonte <tag@pault.ag>
2013-07-24 16:42:19 -0400

    Update merge to not dupe a membership

--- pupa/utils/merge.py
+++ pupa/utils/merge.py
@@ -14,9 +14,17 @@ def match_membership(membership, other_entity):
 
     unmatched = membership.pop("unmatched_legislator")
 
+    l = lambda org, member: db.memberships.find_one({"person_id": member,
+                                                     "organization_id": org})
+
     def match_person(membership, person):
         if person['_type'] != 'person':
             raise ValueError("Matching a person, but not given a person.")
+
+        m = l(membership['organization_id'], person['_id'])
+        if m:
+            db.memberships.remove({"_id": membership['_id']})
+            membership = m
 
         name = unmatched['name']
         if 'other_names' not in person:
@@ -35,6 +43,11 @@ def match_membership(membership, other_entity):
             raise ValueError("Matching an organization, but not given an "
                              "organization.")
         membership['organization_id'] = org['_id']
+
+        m = l(membership['organization_id'], membership['person_id'])
+        if m:
+            db.memberships.remove({"_id": membership['_id']})
+            membership = m
 
         db.organizations.save(org)
         db.memberships.save(membership)
