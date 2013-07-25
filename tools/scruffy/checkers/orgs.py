deed5ff2f0a851c74e56d90dd9cea7ab497401db
Paul Tagliamonte <tag@pault.ag>
2013-07-25 13:43:06 -0400

    add a check for two jurisdiction orgs in the same jurisdiction

--- tools/scruffy/checkers/orgs.py
+++ tools/scruffy/checkers/orgs.py
@@ -7,6 +7,15 @@ def check(db):
         for check in common_checks(org, 'organization', 'organizations'):
             yield check
 
+        if db.organizations.find({
+            "classification": "jurisdiction",
+            "jurisdiction_id": org['jurisdiction_id']
+        }).count() != 1:
+            yield Check(collection='organizations',
+                        id=org['_id'],
+                        tagname='jurisdiction_id-has-two-jurisdiction-orgs',
+                        severity='critical')
+
         jid = org.get('jurisdiction_id')
         if jid is None:
             yield Check(collection='organizations',
