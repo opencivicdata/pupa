b1c430d26d909a85df86da89d8cb48cdb2ccfc0d
Paul Tagliamonte <tag@pault.ag>
2013-07-25 13:56:16 -0400

    Move the check behind the jid check

--- tools/scruffy/checkers/orgs.py
+++ tools/scruffy/checkers/orgs.py
@@ -7,6 +7,14 @@ def check(db):
         for check in common_checks(org, 'organization', 'organizations'):
             yield check
 
+        jid = org.get('jurisdiction_id')
+        if jid is None:
+            yield Check(collection='organizations',
+                        id=org['_id'],
+                        tagname='org-has-no-jurisdiction',
+                        severity='critical')
+            continue
+
         if db.organizations.find({
             "classification": "jurisdiction",
             "jurisdiction_id": org['jurisdiction_id']
@@ -15,14 +23,6 @@ def check(db):
                         id=org['_id'],
                         tagname='jurisdiction_id-has-two-jurisdiction-orgs',
                         severity='critical')
-
-        jid = org.get('jurisdiction_id')
-        if jid is None:
-            yield Check(collection='organizations',
-                        id=org['_id'],
-                        tagname='org-has-no-jurisdiction',
-                        severity='critical')
-            continue
 
         if org.get('parent_id'):
             yield Check(collection='organizations',
