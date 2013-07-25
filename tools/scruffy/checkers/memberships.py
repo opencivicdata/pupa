85aa200feff23f30716fe00083c556b8b9c1c947
Paul Tagliamonte <tag@pault.ag>
2013-07-25 15:13:17 -0400

    Get double-null memberships

--- tools/scruffy/checkers/memberships.py
+++ tools/scruffy/checkers/memberships.py
@@ -3,17 +3,37 @@ from .common import common_checks, resolve
 import datetime
 
 
+ERRORFUL_MEMBERSHIPS = []
+
+
 def check(db):
-    for membership in db.memberships.find({"person_id": None}):
+    for membership in db.memberships.find({"person_id": None,
+                                           "organization_id": None}):
+        ERRORFUL_MEMBERSHIPS.append(membership['_id'])
+
         yield Check(collection='memberships',
                     id=membership['_id'],
-                    tagname='membership-without-a-person',
+                    tagname='membership-without-any-relation',
                     severity='critical',
                     data=membership)
 
+
+    for membership in db.memberships.find({"person_id": None}):
+        if membership['_id'] in ERRORFUL_MEMBERSHIPS:
+            continue
+
+        yield Check(collection='memberships',
+                    id=membership['_id'],
+                    tagname='membership-without-a-person',
+                    severity='important',
+                    data=membership)
+
     for membership in db.memberships.find({"organization_id": None}):
+        if membership['_id'] in ERRORFUL_MEMBERSHIPS:
+            continue
+
         yield Check(collection='memberships',
                     id=membership['_id'],
                     tagname='membership-without-an-organization',
-                    severity='critical',
+                    severity='important',
                     data=membership)
