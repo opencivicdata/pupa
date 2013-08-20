f169b28a392b17035c51f4a513f2275bb933ec0d
twneale <twneale@gmail.com>
2013-08-20 14:35:52 -0400

    Dedent metadata so it displays in narrower view for Sphinx docs.

--- example/__init__.py
+++ example/__init__.py
@@ -6,19 +6,26 @@ class Example(Jurisdiction):
     jurisdiction_id = 'ex'
 
     def get_metadata(self):
-        return {'name': 'Example',
-                'legislature_name': 'Example Legislature',
-                'legislature_url': 'http://example.com',
-                'terms': [{'name': '2013-2014', 'sessions': ['2013'],
-                           'start_year': 2013, 'end_year': 2014
-                          }],
-                'provides': ['people'],
-                'parties': [{'name': 'Independent' },
-                            {'name': 'Green' },
-                            {'name': 'Bull-Moose'}
-                           ],
-                'session_details': {'2013': {'_scraped_name': '2013'}},
-                'feature_flags': [],
+        return {
+            'name': 'Example',
+            'legislature_name': 'Example Legislature',
+            'legislature_url': 'http://example.com',
+            'terms': [{
+                'name': '2013-2014',
+                'sessions': ['2013'],
+                'start_year': 2013,
+                'end_year': 2014
+                }],
+            'provides': ['people'],
+            'parties': [
+                {'name': 'Independent' },
+                {'name': 'Green' },
+                {'name': 'Bull-Moose'}
+               ],
+            'session_details': {
+                '2013': {'_scraped_name': '2013'}
+                },
+            'feature_flags': [],
                }
 
     def get_scraper(self, term, session, scraper_type):
