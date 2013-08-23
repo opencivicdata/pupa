37ad455bdabd7a93254f17c1838e526bd6d77c66
twneale <twneale@gmail.com>
2013-08-23 10:42:12 -0400

    Add sources to example scrape.

--- example/people.py
+++ example/people.py
@@ -8,16 +8,19 @@ class PersonScraper(Scraper):
         # committee
         tech = Organization('Technology', classification='committee')
         tech.add_post('Chairman', 'chairman')
+        tech.add_source('https://example.com')
         yield tech
 
         # subcommittee
         ecom = Organization('Subcommittee on E-Commerce',
                             parent=tech,
                             classification='committee')
+        ecom.add_source('https://example.com')
         yield ecom
 
         p = Person('Paul Tagliamonte', district='6', chamber='upper',
                        party='Independent')
         p.add_committee_membership('Finance')
         p.add_membership(tech, role='chairman')
+        p.add_source('https://example.com')
         yield p
