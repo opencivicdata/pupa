12e3d739528c015ffc0801de282c9fc43e7134ed
Paul Tagliamonte <tag@pault.ag>
2013-08-30 17:05:18 -0400

    Add template check

--- tools/scruffy/checkers/meta.py
+++ tools/scruffy/checkers/meta.py
@@ -4,6 +4,14 @@ from .common import common_checks
 
 def check(db):
     for meta in db.metadata.find():
+        if (meta['_id'] ==
+                "ocd-jurisdiction/country:us/state:ex/place:example"):
+
+            yield Check(collection='metadata',
+                        id=meta['_id'],
+                        tagname='template-data-in-metadata',
+                        severity='grave')
+
         for check in common_checks(meta, 'metadata', 'metadata'):
             yield check
 
