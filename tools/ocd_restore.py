5283bddb36bf4016609c130ddbe63cb234dceb73
Paul Tagliamonte <tag@pault.ag>
2013-12-04 15:19:39 -0500

    Add more to the restore script.

--- tools/ocd_restore.py
+++ tools/ocd_restore.py
@@ -4,6 +4,7 @@ from contextlib import contextmanager
 from pymongo import Connection
 import argparse
 import json
+import sys
 import os
 
 parser = argparse.ArgumentParser(description='Re-convert a jurisdiction.')
@@ -27,7 +28,15 @@ args = parser.parse_args()
 
 connection = Connection(args.server, args.port)
 db = getattr(connection, args.database)
-jurisdiction = args.jurisdiction
+
+
+TABLES = {
+    "ocd-jurisdiction": db.jurisdictions,
+    "ocd-bill": db.bills,
+    "ocd-organization": db.organizations,
+    "ocd-person": db.people,
+    "ocd-vote": db.votes,
+}
 
 
 @contextmanager
@@ -40,5 +49,17 @@ def cd(path):
         os.chdir(pop)
 
 
+def insert(obj):
+    id_ = obj['_id']
+    etype, _ = id_.split("/", 1)
+    sys.stdout.write(etype.split("-")[1][0].lower())
+    sys.stdout.flush()
+    return TABLES[etype].save(obj)
+
+
 with cd(args.root):
-    print os.getcwd()
+    # OK. Let's load stuff up.
+    for path, dirs, nodes in os.walk("."):
+        for entry in (os.path.join(path, x) for x in nodes):
+            data = json.load(open(entry, 'r'))
+            insert(data)
