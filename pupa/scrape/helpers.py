1965b1db79758a53eb45f81bb25e83c4d09b95af
James McKinney <james@slashpoundbang.com>
2014-01-12 23:37:15 -0500

    Add a slot for legislator role

--- pupa/scrape/helpers.py
+++ pupa/scrape/helpers.py
@@ -6,7 +6,7 @@ from pupa.models.membership import Membership
 
 class Legislator(Person):
     _is_legislator = True
-    __slots__ = ('post_id', 'party', 'chamber', '_contact_details')
+    __slots__ = ('post_id', 'party', 'chamber', '_contact_details', 'role')
 
     def __init__(self, name, post_id, party=None, chamber=None, **kwargs):
         super(Legislator, self).__init__(name, **kwargs)
