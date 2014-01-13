fbc4247fc7b7d36286c3f25e6ae71dfb7ebb2d39
James McKinney <james@slashpoundbang.com>
2014-01-13 15:31:11 -0500

    Use new-style metadata in example

--- example/__init__.py
+++ example/__init__.py
@@ -5,28 +5,23 @@ from .people import PersonScraper
 
 class Example(Jurisdiction):
     jurisdiction_id = 'ocd-jurisdiction/country:us/state:ex/place:example'
-
-    def get_metadata(self):
-        return {
-            'name': 'Example Legislature',
-            'url': 'http://example.com',
-            'terms': [{
-                'name': '2013-2014',
-                'sessions': ['2013'],
-                'start_year': 2013,
-                'end_year': 2014
-                }],
-            'provides': ['people'],
-            'parties': [
-                {'name': 'Independent' },
-                {'name': 'Green' },
-                {'name': 'Bull-Moose'}
-               ],
-            'session_details': {
-                '2013': {'_scraped_name': '2013'}
-                },
-            'feature_flags': [],
-               }
+    name = 'Example Legislature'
+    url = 'http://example.com'
+    terms = [{
+        'name': '2013-2014',
+        'sessions': ['2013'],
+        'start_year': 2013,
+        'end_year': 2014
+    }]
+    provides = ['people'],
+    parties = [
+        {'name': 'Independent' },
+        {'name': 'Green' },
+        {'name': 'Bull-Moose'}
+    ]
+    session_details = {
+        '2013': {'_scraped_name': '2013'}
+    }
 
     def get_scraper(self, term, session, scraper_type):
         if scraper_type == 'people':
