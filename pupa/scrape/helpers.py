0d60c7833e59d2f61149d2f21be687bca5a70c9d
James McKinney <james@slashpoundbang.com>
2014-01-13 17:46:57 -0500

    Underscore _role, since it gets deleted

--- pupa/scrape/helpers.py
+++ pupa/scrape/helpers.py
@@ -6,7 +6,7 @@ from pupa.models.membership import Membership
 
 class Legislator(Person):
     _is_legislator = True
-    __slots__ = ('post_id', 'party', 'chamber', '_contact_details', 'role')
+    __slots__ = ('post_id', 'party', 'chamber', '_contact_details', '_role')
 
     def __init__(self, name, post_id, party=None, chamber=None, role='member', **kwargs):
         super(Legislator, self).__init__(name, **kwargs)
@@ -14,7 +14,7 @@ class Legislator(Person):
         self.party = party
         self.chamber = chamber
         self._contact_details = []
-        self.role = role
+        self._role = role
 
     def add_contact(self, type, value, note):
         self._contact_details.append({'type': type, 'value': value,
