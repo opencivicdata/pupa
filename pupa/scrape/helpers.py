a346ddfd7517c68c115d72df7f514aa2bf807b84
twneale <twneale@gmail.com>
2014-06-17 11:48:37 -0400

    Optionally store start/end dates when legislator obj is initialized. Then use them to create default role.

--- pupa/scrape/helpers.py
+++ pupa/scrape/helpers.py
@@ -7,12 +7,16 @@ class Legislator(Person):
     """
     Legislator is a special case of Person that has a district, party, and perhaps a chamber
     """
-    def __init__(self, name, district, party=None, chamber='', role='member', **kwargs):
+    def __init__(
+            self, name, district, party=None, chamber='', role='member',
+            start_date=None, end_date=None, **kwargs):
         super(Legislator, self).__init__(name, **kwargs)
         self._district = district
         self._party = party
         self._chamber = chamber
         self._role = role
+        self._start_date = start_date
+        self._end_date = end_date
 
     def pre_save(self, jurisdiction_id):
         # before saving create a membership to the current jurisdiction
@@ -24,7 +28,9 @@ class Legislator(Person):
             person_id=self._id,
             organization_id=make_psuedo_id(classification="legislature", chamber=self._chamber),
             post_id=make_psuedo_id(**post_kwargs),
-            role=self._role)
+            role=self._role,
+            start_date=self._start_date,
+            end_date=self._end_date)
         self._related.append(membership)
 
         # create a party membership
