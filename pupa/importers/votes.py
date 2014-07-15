0ce0195feafd9e0875dc7f9f5a129f5772df6ed5
James Turk <james.p.turk@gmail.com>
2014-07-15 18:42:24 -0400

    use bill_id & identifier together if they exist

--- pupa/importers/votes.py
+++ pupa/importers/votes.py
@@ -18,12 +18,12 @@ class VoteImporter(BaseImporter):
         self.votes_to_delete = set()
 
     def get_object(self, vote):
-        if vote['identifier']:
-            spec = {
-                'legislative_session': vote['legislative_session'],
-                'identifier': vote['identifier'],
-            }
-        elif vote['bill_id']:
+        spec = {'legislative_session': vote['legislative_session']}
+
+        if not vote['identifier'] and not vote['bill_id']:
+            raise ValueError('attempt to save a Vote without an "identifier" or "bill_id"')
+
+        if vote['bill_id']:
             if vote['bill_id'] not in self.seen_bill_ids:
                 self.seen_bill_ids.add(vote['bill_id'])
                 # keep a list of all the vote ids that should be deleted
@@ -31,15 +31,19 @@ class VoteImporter(BaseImporter):
                     self.model_class.objects.filter(bill_id=vote['bill_id']).values_list(
                         'id', flat=True)
                 )
-            spec = {
-                'legislative_session': vote['legislative_session'],
-                'bill_id': vote['bill_id'],
+            spec['bill_id'] = vote['bill_id']
+
+        if vote['identifier']:
+            # if there's an identifier, just use it and the bill_id and the session
+            spec['identifier'] = vote['identifier']
+        else:
+            # otherwise use the motion, start_date, and org as well
+            spec.update({
                 'motion_text': vote['motion_text'],
                 'start_date': vote['start_date'],
                 'organization_id': vote['organization_id']
-            }
-        else:
-            raise ValueError('attempt to save a Vote without an "identifier" or "bill_id"')
+            })
+
         return self.model_class.objects.get(**spec)
 
     def prepare_for_db(self, data):
