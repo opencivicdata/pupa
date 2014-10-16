3061461cf358646277d0f155bbba4f754acfc903
James McKinney <james@slashpoundbang.com>
2014-10-16 17:51:11 -0400

    get_jurisdiction returns proper Jurisdictions, not abstract Jurisdictions without division_id or classification

--- pupa/cli/commands/update.py
+++ pupa/cli/commands/update.py
@@ -86,22 +86,10 @@ class Command(BaseCommand):
         # get the jurisdiction object
         module = importlib.import_module(module_name)
         for obj in module.__dict__.values():
-            # ensure we're sealing with a subclass of Jurisdiction
-            if isinstance(obj, type) and issubclass(obj, Jurisdiction):
-                if obj == Jurisdiction:
-                    # We're looking at the imported Jurisdiction.
-                    continue
-                if getattr(obj, 'division_id', None):
-                    if obj.classification is None:
-                        raise CommandError(
-                            "classification isn't set on '"
-                            + obj.__name__ + "'"
-                        )
-                    return obj()
-                else:
-                    raise CommandError("Jurisdiction subclass '" + obj.__name__
-                                       + "' is missing a division_id")
-        raise CommandError('unable to import Jurisdiction subclass from ' + module_name)
+            # ensure we're dealing with a subclass of Jurisdiction
+            if isinstance(obj, type) and issubclass(obj, Jurisdiction) and getattr(obj, 'division_id', None) and obj.classification:
+                return obj()
+        raise CommandError('Unable to import Jurisdiction subclass from ' + module_name + '. Jurisdiction subclass may be missing a division_id and classification.')
 
     def do_scrape(self, juris, args, scrapers):
         # make output and cache dirs
