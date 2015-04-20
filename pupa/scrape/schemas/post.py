e8f68af1e58fa29bdf7f1f8716f71e58bc36082a
Miles Watkins <miles.w.watkins@gmail.com>
2015-04-20 11:05:23 -0400

    Fix schema update for Post

--- pupa/scrape/schemas/post.py
+++ pupa/scrape/schemas/post.py
@@ -8,7 +8,7 @@ schema = {
         "division_id": {"type": ["null", "string"],},
         "start_date": fuzzy_date_blank,
         "end_date": fuzzy_date_blank,
-        "num_seats": { "type": "int" },
+        "num_seats": { "type": "integer" },
         "contact_details": contact_details,
         "links": links,
         "extras": extras,
