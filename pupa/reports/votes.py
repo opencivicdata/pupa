ca5d949357befc5b37161fcf48fc7618e2afb532
Miles Watkins <miles.w.watkins@gmail.com>
2015-05-11 11:38:11 -0400

    Complete check for invalid vote counts

--- pupa/reports/votes.py
+++ pupa/reports/votes.py
@@ -1,17 +1,24 @@
-from django.db.models import Count
-from django.db.models import Q
-from opencivicdata.models.vote import VoteEvent
-
-from .utils import percentage
+from opencivicdata.models.vote import VoteEvent, VoteCount, PersonVote
 
 
 def vote_report(jurisdiction):
     report = {}
 
     votes = VoteEvent.objects.filter(organization__jurisdiction=jurisdiction)
-    total = votes.count()
 
-    report['invalid'] = percentage(
-        votes.annotate(yeses=Count('counts__yes')).filter(Q(votes='counts__yes') | Q())
+    report['invalid'] = 0
+    for vote in votes:
+        person_votes = PersonVote.objects.filter(vote=vote)
+        vote_counts = VoteCount.objects.filter(vote=vote)
+
+        is_invalid = False
+        for vote_option in vote_counts.values_list('option', flat=True):
+            roll_call_count = person_votes.filter(option=vote_option).count()
+            official_count = vote_counts.get(option=vote_option).value
+            if roll_call_count != official_count:
+                is_invalid = True
+
+        if is_invalid:
+            report['invalid'] += 1
 
     return report
