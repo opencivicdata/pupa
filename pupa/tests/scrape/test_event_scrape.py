b13dcc7243041d17c002245b9c9c6027d0a8f412
Clayton Dunwell <clayton.dunwell@gmail.com>
2015-05-18 16:09:53 -0400

    add test for agenda add_vote method

--- pupa/tests/scrape/test_event_scrape.py
+++ pupa/tests/scrape/test_event_scrape.py
@@ -50,6 +50,16 @@ def test_agenda_add_person():
     e.validate()
 
 
+def test_agenda_add_vote():
+    e = event_obj()
+    agenda = e.add_agenda_item("foo bar")
+    assert agenda['related_entities'] == []
+    
+    agenda.add_vote(vote='Roll no. 12')
+    assert len(e.agenda[0]['related_entities']) == 1
+    e.validate()
+
+
 def test_agenda_add_subject():
     e = event_obj()
     agenda = e.add_agenda_item("foo bar")
