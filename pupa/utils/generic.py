b815eadef7c0a983fdea8728fe290fe67a8fcc81
James Turk <james.p.turk@gmail.com>
2015-09-14 10:15:45 -0400

    datetime.date doesn't have tzinfo

--- pupa/utils/generic.py
+++ pupa/utils/generic.py
@@ -50,11 +50,13 @@ class JSONEncoderPlus(json.JSONEncoder):
     JSONEncoder that encodes datetime objects as Unix timestamps.
     """
     def default(self, obj, **kwargs):
-        if isinstance(obj, (datetime.date, datetime.datetime)):
+        if isinstance(obj, datetime.datetime):
             if obj.tzinfo is None:
                 raise TypeError(
                     "date '%s' is not fully timezone qualified." % (obj))
             obj = obj.astimezone(pytz.UTC)
+            return "{}".format(obj.isoformat())
+        elif isinstance(obj, datetime.date):
             return "{}".format(obj.isoformat())
         return super(JSONEncoderPlus, self).default(obj, **kwargs)
 
