aed47e160a1407df3b24058bbe1b172cdfbc12a5
Forest Gregg <fgregg@uchicago.edu>
2015-12-16 13:43:12 -0600

    additional test

--- pupa/tests/scrape/test_people_org_scrape.py
+++ pupa/tests/scrape/test_people_org_scrape.py
@@ -2,6 +2,7 @@ import pytest
 from pupa.scrape import Person, Organization, Membership, Post
 from pupa.utils import get_pseudo_id
 from validictory import ValidationError
+import datetime
 
 
 def test_basic_post():
@@ -49,12 +50,13 @@ def test_person_add_membership():
     p = Person('Bob B. Bear')
     p.add_source('http://example.com')
     o = Organization('test org', classification='unknown')
-    p.add_membership(o, role='member', start_date='2007')
+    p.add_membership(o, role='member', start_date='2007', end_date=datetime.date(2015, 5, 8))
     assert len(p._related) == 1
     p._related[0].validate()
     assert p._related[0].person_id == p._id
     assert p._related[0].organization_id == o._id
     assert p._related[0].start_date == '2007'
+    assert p._related[0].end_date == datetime.date(2015, 5, 8)
 
 
 def test_basic_organization():
