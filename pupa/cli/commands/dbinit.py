c67b440d9c9e0c902f87527562853b3ab21e95d6
James Turk <james.p.turk@gmail.com>
2017-02-20 22:02:34 -0500

    pupa dbinit --reset needs to drop pupa_ tables too

--- pupa/cli/commands/dbinit.py
+++ pupa/cli/commands/dbinit.py
@@ -24,10 +24,11 @@ def drop_tables(skip_divisions=False):
     tables = connection.introspection.table_names()
     cursor = connection.cursor()
     for table in tables:
-        if table.startswith('opencivicdata_'):
+        if table.startswith(('opencivicdata_', 'pupa_')):
             print('dropping table ' + table)
             cursor.execute("DROP TABLE IF EXISTS {} CASCADE;".format(table))
         cursor.execute("DELETE FROM django_migrations WHERE app='opencivicdata';")
+        cursor.execute("DELETE FROM django_migrations WHERE app='pupa';")
 
 
 class Command(BaseCommand):
