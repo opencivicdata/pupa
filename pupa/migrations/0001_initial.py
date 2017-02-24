5b21ce6b2acf557a3f7e147bf2e04bbdb46ce075
James McKinney <james@slashpoundbang.com>
2017-02-23 22:46:03 -0500

    Fix migration dependencies to avoid irresolvable django.db.migrations.exceptions.InconsistentMigrationHistory

--- pupa/migrations/0001_initial.py
+++ pupa/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('opencivicdata', '__latest__'),
+        ('opencivicdata', '0001_initial'),
     ]
 
     operations = [
