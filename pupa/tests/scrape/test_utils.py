371227bfcea1755f775176c6e834f56fe30fbc51
James Turk <james.p.turk@gmail.com>
2017-05-23 23:39:57 -0700

    flake8 pupa/tests/scrape

--- pupa/tests/scrape/test_utils.py
+++ pupa/tests/scrape/test_utils.py
@@ -2,8 +2,10 @@ import pytest
 
 from pupa.cli.commands.update import override_settings
 
+
 class _Settings:
     pass
+
 
 @pytest.fixture
 def settings():
@@ -12,12 +14,14 @@ def settings():
     ret.baz = 'bob'
     return ret
 
+
 def test_override_settings(settings):
     with override_settings(settings, {'baz': 'fez'}):
         assert settings.foo == 'bar'
         assert settings.baz == 'fez'
     assert settings.foo == 'bar'
     assert settings.baz == 'bob'
+
 
 def test_override_settings_unset(settings):
     with override_settings(settings, {'qux': 'fez'}):
