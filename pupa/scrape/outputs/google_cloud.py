a5bc525666e2bbe6cfc98a197083aaeecd58b38a
Saxon Stiller <saxon.stiller@gmail.com>
2017-12-06 02:14:02 -0500

    oops, forgot to specify keyword for arg

--- pupa/scrape/outputs/google_cloud.py
+++ pupa/scrape/outputs/google_cloud.py
@@ -44,8 +44,8 @@ class GoogleCloudPubSub():
 
         credentials = service_account.Credentials.from_service_account_info(
             info,
-            ['https://www.googleapis.com/auth/cloud-platform',
-             'https://www.googleapis.com/auth/pubsub'])
+            scopes=['https://www.googleapis.com/auth/cloud-platform',
+                    'https://www.googleapis.com/auth/pubsub'])
 
         self.publisher = pubsub.PublisherClient(credentials=credentials)
 
