a01dc61a84dc6690b0227f3e8a141a1901b5bea7
doubleswirve <saxon.stiller@gmail.com>
2018-01-22 00:09:31 -0500

    smore patches

--- pupa/scrape/outputs/google_cloud_pubsub.py
+++ pupa/scrape/outputs/google_cloud_pubsub.py
@@ -20,12 +20,12 @@ class GoogleCloudPubSub(Output):
         publisher = pubsub.PublisherClient()
         topic_path = publisher.topic_path(self.project, self.topic_name)
 
-        self.scraper.info('save %s %s to topic %s', obj._type, obj, self.topic_path)
+        self.scraper.info('save %s %s to topic %s', obj._type, obj, topic_path)
         self.scraper.debug(json.dumps(OrderedDict(sorted(obj.as_dict().items())),
                            cls=utils.JSONEncoderPlus,
                            indent=4, separators=(',', ': ')))
 
-        self.add_output_name(obj, self.topic_path)
+        self.add_output_name(obj, topic_path)
         obj_str = self.stringify_obj(obj, True, True)
 
         publisher.publish(topic_path, obj_str.encode('utf-8'))
