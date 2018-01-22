1c832dd8f7364469da3c7a8af798cdc58d5019e0
doubleswirve <saxon.stiller@gmail.com>
2018-01-22 01:24:05 -0500

    remove http version testing and revert to instantiating the publisher client just once in __init__

--- pupa/scrape/outputs/google_cloud_pubsub.py
+++ pupa/scrape/outputs/google_cloud_pubsub.py
@@ -7,29 +7,24 @@ from pupa.scrape.outputs.output import Output
 
 from google.cloud import pubsub
 
-import http.client
-
 
 class GoogleCloudPubSub(Output):
 
     def __init__(self, scraper):
         super().__init__(scraper)
 
-        self.project = os.environ.get('GOOGLE_CLOUD_PROJECT')
-        self.topic_name = os.environ.get('GOOGLE_CLOUD_PUBSUB_TOPIC')
+        project = os.environ.get('GOOGLE_CLOUD_PROJECT')
+        topic_name = os.environ.get('GOOGLE_CLOUD_PUBSUB_TOPIC')
+        self.publisher = pubsub.PublisherClient()
+        self.topic_path = self.publisher.topic_path(project, topic_name)
 
     def handle_output(self, obj):
-        self.scraper.info('{} {}'.format(http.client.HTTPConnection._http_vsn,
-                                         http.client.HTTPConnection._http_vsn_str))
-        publisher = pubsub.PublisherClient()
-        topic_path = publisher.topic_path(self.project, self.topic_name)
-
-        self.scraper.info('save %s %s to topic %s', obj._type, obj, topic_path)
+        self.scraper.info('save %s %s to topic %s', obj._type, obj, self.topic_path)
         self.scraper.debug(json.dumps(OrderedDict(sorted(obj.as_dict().items())),
                            cls=utils.JSONEncoderPlus,
                            indent=4, separators=(',', ': ')))
 
-        self.add_output_name(obj, topic_path)
+        self.add_output_name(obj, self.topic_path)
         obj_str = self.stringify_obj(obj, True, True)
 
-        publisher.publish(topic_path, obj_str.encode('utf-8'))
+        self.publisher.publish(self.topic_path, obj_str.encode('utf-8'))
