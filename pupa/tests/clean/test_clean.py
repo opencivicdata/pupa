1ef9a90062c5ca469b08a44e4e82aeaab1c97e6b
msj <monkruman.st.jules@datamde.us>
2023-03-14 14:36:17 -0400

    Refactor clean tests

--- pupa/tests/clean/test_clean.py
+++ pupa/tests/clean/test_clean.py
@@ -1,10 +1,12 @@
 import pytest
+import argparse
+
 from datetime import datetime, timezone, timedelta
 from freezegun import freeze_time
 
 from opencivicdata.core.models import Person, Organization, Jurisdiction, Division
 
-from pupa.cli.commands.clean import get_stale_objects, remove_stale_objects
+from pupa.cli.commands.clean import Command
 
 
 def create_jurisdiction():
@@ -12,8 +14,24 @@ def create_jurisdiction():
     return Jurisdiction.objects.create(id="jid", division_id="ocd-division/country:us")
 
 
+@pytest.fixture
+def subparsers():
+    parser = argparse.ArgumentParser("pupa", description="pupa CLI")
+    parser.add_argument("--debug", action="store_true", help="open debugger on error")
+    parser.add_argument(
+        "--loglevel",
+        default="INFO",
+        help=(
+            "set log level. options are: "
+            "DEBUG|INFO|WARNING|ERROR|CRITICAL "
+            "(default is INFO)"
+        ),
+    )
+    return parser.add_subparsers(dest="subcommand")
+
+
 @pytest.mark.django_db
-def test_get_stale_objects():
+def test_get_stale_objects(subparsers):
     j = create_jurisdiction()
     o = Organization.objects.create(name="WWE", jurisdiction_id="jid")
     p = Person.objects.create(name="George Washington", family_name="Washington")
@@ -26,11 +44,11 @@ def test_get_stale_objects():
         p = Person.objects.create(name="Thomas Jefferson", family_name="Jefferson")
         j.save()
         p.memberships.create(organization=o)
-        assert set(get_stale_objects(7)) == expected_stale_objects
+        assert set(Command(subparsers).get_stale_objects(7)) == expected_stale_objects
 
 
 @pytest.mark.django_db
-def test_remove_stale_objects():
+def test_remove_stale_objects(subparsers):
     j = create_jurisdiction()
     o = Organization.objects.create(name="WWE", jurisdiction_id="jid")
     p = Person.objects.create(name="George Washington", family_name="Washington")
@@ -45,7 +63,7 @@ def test_remove_stale_objects():
 
         j.save()
 
-        remove_stale_objects(7)
+        Command(subparsers).remove_stale_objects(7)
         for obj in expected_stale_objects:
             was_deleted = not type(obj).objects.filter(id=obj.id).exists()
             assert was_deleted
