9e8f1c7591c9c13857a05efb51a5035855031ef0
Hannah Cushman Garland <hannah.cushman@datamade.us>
2024-09-11 15:18:30 -0500

    Update pupa clean so that Jurisdiction, Division, and Post objects are not deleted

--- pupa/tests/django_settings.py
+++ pupa/tests/django_settings.py
@@ -1,4 +1,6 @@
 # django settings for tests
+import os
+
 SECRET_KEY = 'test'
 INSTALLED_APPS = ('django.contrib.contenttypes',
                   'opencivicdata.core.apps.BaseConfig',
@@ -7,11 +9,11 @@ INSTALLED_APPS = ('django.contrib.contenttypes',
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'test',
-        'USER': 'test',
-        'PASSWORD': 'test',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'NAME': os.getenv('POSTGRES_DB', 'test'),
+        'USER': os.getenv('POSTGRES_USER', 'test'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'test'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
     }
 }
 MIDDLEWARE_CLASSES = ()
